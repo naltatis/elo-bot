0c9c02b09cbf2e018b4d9b1d197acedf25d02f18
Michael Geers <michael@geers.tv>
2011-07-13 22:28:39 +0200

    first shot

--- bot.rb
+++ bot.rb
@@ -1,95 +1,84 @@
 require 'elo'
+require 'json'
+require 'net/http'
 require 'pp'
 
-class Importer
-  def run(from, to)
-    matches = []
-    (from..to).each do |year|
-      puts year
-      matches = matches + read(year)
-    end
-    puts "#{matches.size} games"
-    matches
-  end
-  
-  private
-
-  def read(year)
-    matches = []
-    
-    f = File.open("data/#{year}.txt", "r") 
-    f.each_line do |line|
-      entry = parse_line(line)
-      matches << entry if entry
-    end
-    matches
-  end
-  
-  def parse_line line
-    if line.include? 'Spieltag'
-      @group = line[3,3].strip
-    else
-      @date = line[6,10] unless line[6,10].strip.empty?
-      @time = line[17,5] unless line[17,5].strip.empty?
-      
-      first, last = line.split ' - '
-
-      entry = {:group => @group, :date => @date, :time => @time, :year => @year}
-      result = last.match /(.*)\w*(\d):(\d)/
-      entry[:team2] = result[1].strip
-      entry[:points1] = result[2].strip.to_i
-      entry[:points2] = result[3].strip.to_i
-      entry[:team1] = first[-20,20].strip
-    
-      entry[:diff] = entry[:points1] - entry[:points2]
-    
-      entry[:tendency] = 'home' if entry[:diff] > 0
-      entry[:tendency] = 'guest' if entry[:diff] < 0
-      entry[:tendency] = 'draw' if entry[:diff] == 0
-    end
-    
-    entry
-  end
-end
-
-
-class Ratings
+class EloRatings
   def initialize
     @teams = {}
   end
   
-  def process matches
+  def process (matches)
     matches.each do |match|
-      t1 = team match[:team1]
-      t2 = team match[:team2]
+      t1 = team match['id_team1']
+      t2 = team match['id_team2']
       
-      g = t1.versus(t2)
+      we = estimate(t1.rating, t2.rating)
+
+      yield match, we
       
-      dr = t1.rating - t2.rating + 100
-      we = 1.0 / (10 ** (-1*dr/400.0) + 1)
-      puts "#{we} #{match[:tendency]}"
+      calculate(t1, t2, match)
       
-      if match[:tendency] == 'home'
-        g.winner = t1
-      elsif match[:tendency] == 'guest'
-        g.winner = t2
-      else
-        g.draw
-      end
     end
-    @teams.each { |k,v| puts "elo: #{v.rating}\tgames: #{v.games_played}\tteam: #{k}"}
   end
   
   private
-  def team (name)
-    @teams[name] ||= Elo::Player.new
+  
+  # see http://de.wikipedia.org/wiki/World_Football_Elo_Ratings
+  def estimate (rating1, rating2)
+    dr = rating1 - rating2 + 100
+    we = 1.0 / (10 ** (-1*dr/400.0) + 1)
+    we
+  end
+  
+  def diff (match)
+    match['points_team1'].to_i - match['points_team2'].to_i
+  end
+  
+  def calculate (t1, t2, match)
+    g = t1.versus(t2)
+    if diff(match) > 0
+      g.winner = t1
+    elsif diff(match) < 0
+      g.winner = t2
+    else
+      g.draw
+    end
+  end
+  
+  def team (id)
+    @teams[id] ||= Elo::Player.new
+  end
+end
+
+class Botliga
+  def initialize(token)
+    @http = Net::HTTP.new('botliga.de', 80)
+    @token = token
+  end
+  
+  def post(match_id, result)
+    @http.post('/api/guess',"match_id=#{match_id}&result=#{result}&token=#{@token}")
   end
 end
 
 
+file = File.open("data/matches.json", "rb")
+matches = JSON.parse(file.read)
 
-i = Importer.new
-matches = i.run(2007, 2010)
+liga = Botliga.new(ARGV[0])
 
-r = Ratings.new
-r.process matches
+r = EloRatings.new
+r.process(matches) do |match, we|
+  if match['league_saison'] == '2010'
+    if we < 0.5
+      result = '0:1'
+    else
+      result = '2:1'
+    end
+      
+    pp liga.post(match['match_id'], result)
+  end
+end
+
+puts 
