c198830feb5a2c346d1cdd6266ba2f867e4b2fb5
Michael Geers <michael.geers@neuland-bfi.de>
2014-03-18 21:09:51 +0100

    include 2013 matches

--- bot.rb
+++ bot.rb
@@ -8,33 +8,33 @@ class EloRatings
   def initialize
     @teams = {}
   end
-  
+
   def process (matches)
     matches.each do |match|
       t1 = team match['hostId']
       t2 = team match['guestId']
-      
+
       we = estimate(t1.rating, t2.rating)
 
       yield match, we
-      
-      calculate(t1, t2, match)      
+
+      calculate(t1, t2, match)
     end
   end
-  
+
   private
-  
+
   # see http://de.wikipedia.org/wiki/World_Football_Elo_Ratings
   def estimate (rating1, rating2)
     dr = rating1 - rating2 + 100
     we = 1.0 / (10 ** (-1*dr/400.0) + 1)
     we
   end
-  
+
   def diff (match)
     match['hostGoals'].to_i - match['guestGoals'].to_i
   end
-  
+
   def calculate (t1, t2, match)
     g = t1.versus(t2)
     if diff(match) > 0
@@ -45,7 +45,7 @@ class EloRatings
       g.draw
     end
   end
-  
+
   def team (id)
     @teams[id] ||= Elo::Player.new
   end
@@ -58,7 +58,7 @@ class Importer
 
   def import
     matches = []
-    (2010..2012).each do |year| 
+    (2010..2013).each do |year|
       puts ">> /api/matches/#{year}"
       response = @http.get("/api/matches/#{year}")
       data = JSON.parse(response.body)
@@ -74,7 +74,7 @@ class Botliga
     #@uri = URI('http://localhost:3000/api/guess')
     @token = token
   end
-  
+
   def post(match_id, result)
     Net::HTTP.post_form(@uri, :match_id => match_id, :result => result, :token => @token)
   end
